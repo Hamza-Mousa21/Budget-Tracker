0e5671b49719b4be5590145cf9fafe76e096ffec
Hamza-Mousa21 <hamza12345wael@gmail.com>
2026-04-02 17:41:14 +0300

    Initial header

---
+++ src/Component/header.jsx
@@ -0,0 +1,39 @@
+const Header=()=>{
+      const date=new Date().toLocaleDateString('en-US', {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+});
+  
+    return (
+        
+        <>
+            <header className="position-sticky p-3 d-flex justify-content-between" style={{top:0}}>
+               <div>
+                <h3>Welcome back, Hamza Mousa</h3>
+                <p style={{color:"gray"}}>{date}</p>
+               </div>
+               <div className="d-flex">  
+                <div>
+                    <i class="bi bi-brightness-high me-3" style={{fontSize:"2rem",
+                        border:"1px solid black",
+                        borderRadius:"8px",
+                        paddingLeft:"6px",
+                        paddingRight:"6px"
+                        }}></i>
+                </div> 
+                <div>
+                    <i class="bi bi-person" style={{fontSize:"2rem",
+                        border:"1px solid black",
+                        borderRadius:"8px",
+                        paddingLeft:"6px",
+                        paddingRight:"6px"
+                        }}></i>
+                </div> 
+               </div>
+            </header>
+        </>
+    )
+}
+export default Header
